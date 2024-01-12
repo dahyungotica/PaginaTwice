03b0a3c537864aaf01fc31b2c46610e3ac5b9592
Brandon <br.cayuela@duocuc.cl>
2024-01-12 02:27:04 -0300

    DesarrolloTwice

--- main.js
+++ main.js
@@ -8,3 +8,12 @@ typewriter
  .typeString('¡One in a million!')
  .pauseFor(200)
  .start();
+
+document.getElementById("twiceButton").addEventListener("click", function() {
+    window.open("https://twice.jype.com/", "_blank");
+});
+  
+document.getElementById("loginButton").addEventListener("click", function() {
+    window.location.href = "form.html";
+});
+  
